c3b5178988df84d48b7a821d57c6f4025d4ab3a7
swapnasahoo <sahooswapnaswarup@gmail.com>
2026-09-07 10:28:39 +0530

    feat: update Input component to support icon and trailing icon

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,20 @@
 import { Eye, EyeOff } from "lucide-react";
-import { useState, type ComponentProps } from "react";
+import { useState, type ComponentProps, type ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
 type InputProps = ComponentProps<"input"> & {
   error?: boolean;
+  icon?: ReactNode;
+  trailingIcon?: ReactNode;
 };
 
-export default function Input({ error, className, ...props }: InputProps) {
+export default function Input({
+  error,
+  icon,
+  trailingIcon,
+  className,
+  ...props
+}: InputProps) {
   const [isPasswordShown, setIsPasswordShown] = useState<boolean>(false);
 
   const isPassword = props.type === "password";
@@ -17,7 +25,8 @@ export default function Input({ error, className, ...props }: InputProps) {
     : "border-border focus:border-foreground";
 
   const classes = twMerge(
-    "bg-transparent text-foreground pl-4 w-full h-10 rounded-md outline-none transition-colors duration-200 ease-in border",
+    "bg-transparent text-foreground w-full h-10 rounded-md outline-none transition-colors duration-200 ease-in border",
+    icon ? "pl-10" : "pl-4",
     className,
     borderClasses,
   );
@@ -25,6 +34,10 @@ export default function Input({ error, className, ...props }: InputProps) {
   return (
     <div className="relative">
       <input {...props} type={inputType} className={classes} />
+      {/* icon (starting) */}
+      {icon && (
+        <span className="absolute text-foreground top-2 left-3">{icon}</span>
+      )}
 
       <button
         onClick={() => setIsPasswordShown(!isPasswordShown)}
@@ -32,6 +45,13 @@ export default function Input({ error, className, ...props }: InputProps) {
       >
         {isPassword && (isPasswordShown ? <Eye /> : <EyeOff />)}
       </button>
+
+      {/* icon (ending) */}
+      {trailingIcon && (
+        <span className="absolute text-foreground top-2 right-3">
+          {trailingIcon}
+        </span>
+      )}
     </div>
   );
 }

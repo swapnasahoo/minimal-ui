1cc0661606266680d9aae2f3076546467bedaaba
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-09-09 01:11:18 +0530

    feat: add track color prop in ProgressBar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -3,15 +3,22 @@ import { twMerge } from "tailwind-merge";
 type ProgressBarProps = {
   value: number;
   max: number;
+  trackColor?: string;
   barColor?: string;
 };
 
 export default function ProgressBar({
   value,
   max,
+  trackColor,
   barColor,
 }: ProgressBarProps) {
   const progress = Math.floor((value / max) * 100);
+
+  const trackClasses = twMerge(
+    "w-full h-2 bg-background/90 rounded-full",
+    trackColor,
+  );
 
   const barClasses = twMerge(
     `w-[${progress}%] h-full bg-foreground rounded-full`,
@@ -19,7 +26,7 @@ export default function ProgressBar({
   );
 
   return (
-    <div className="w-full h-2 bg-background/90 rounded-full">
+    <div className={trackClasses}>
       <div className={barClasses}></div>
     </div>
   );

98fdc776fb62bfbb36237979435c41464f1c63ef
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-09-09 10:35:42 +0530

    feat: add error and label props to TextArea component

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,13 +1,34 @@
 import type { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-type TextAreaProps = ComponentProps<"textarea">;
+type TextAreaProps = ComponentProps<"textarea"> & {
+  label?: string;
+  error?: boolean;
+};
 
-export default function TextArea({ className, ...props }: TextAreaProps) {
+export default function TextArea({
+  label,
+  error,
+  className,
+  ...props
+}: TextAreaProps) {
+  const borderClasses = error
+    ? "border-destructive/80 focus:border-destructive"
+    : "border-border focus:border-foreground";
+
   const classes = twMerge(
     "w-full min-h-20 bg-transparent px-3 py-2 text-sm text-foreground placeholder:text-muted outline-none rounded-md border border-border focus:border-foreground transition-colors duration-200 ease-in",
+    borderClasses,
     className,
   );
 
-  return <textarea {...props} className={classes} />;
+  return (
+    <div>
+      {label && (
+        <label className="text-foreground font-semibold mb-1">{label}</label>
+      )}
+
+      <textarea {...props} className={classes} />
+    </div>
+  );
 }
